aae17c48dbb41930cfd44d49875228c2461b0b11
Abdelrahman Awad <logaretm1@gmail.com>
2018-05-25 06:33:47 +0200

    add basic tests

---
+++ test/Slider.spec.js
@@ -0,0 +1,9 @@
+import { shallowMount } from '@vue/test-utils';
+import Slider from '@/components/Slider';
+
+describe('Slider', () => {
+  test('is a Vue instance', () => {
+    const wrapper = shallowMount(Slider);
+    expect(wrapper.isVueInstance()).toBeTruthy();
+  });
+});
---
+++ test/setupTests.js
@@ -0,0 +1,13 @@
+class Path2D {
+  get path () {
+
+  }
+
+  moveTo () {}
+
+  closePath () {}
+
+  arc () {}
+};
+
+global.Path2D = Path2D;
