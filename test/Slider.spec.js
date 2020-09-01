9f6745dbb5e43d9b55bebc403dedddbccfe628f1
Bhoomi Bhalani <bhoomi.bhalani@robustwealth.com>
2020-09-01 19:01:55 +0530

    fix: unit test

--- test/Slider.spec.js
+++ test/Slider.spec.js
@@ -5,5 +5,6 @@ describe('Slider', () => {
   test('is a Vue instance', () => {
     const wrapper = shallowMount(Slider);
     expect(wrapper.isVueInstance()).toBeTruthy();
+    expect(wrapper.html()).toMatchSnapshot();
   });
 });
